2d5d0145932ce0732abe84f395c3ab0e81144e1b
Rivany Tasik Lalundun_46121120 <rivany018@gmail.com>
2024-12-29 12:48:09 +0800

    Add files via upload

---
+++ inisialisasi.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'inti/kumpulan_function.php';
+require_once 'inti/configurasi.php';
+require_once 'inti/kelola_sesi.php';
+require_once 'inti/controller.php';
+require_once 'inti/model.php';
+require_once 'inti/database.php';
+require_once 'inti/my_root.php';
